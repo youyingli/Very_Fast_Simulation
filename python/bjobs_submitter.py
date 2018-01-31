cc2ffb9a47791d2ae2447c04965c455da697c7f5
Youying <luffy7804@gmail.com>
2018-01-31 22:43:23 +0100

    Update to the stable version

--- python/bjobs_submitter.py
+++ python/bjobs_submitter.py
@@ -48,9 +48,9 @@ def decode_txt (filename) :
     file = open(filename, 'r')
     return file.readlines()
 
-def batch_job_submitter (script_list, queue = '8nh', outlog = '/tmp/junk/log') :
+def batch_job_submitter (script_list, queue) :
     for script in script_list :
-        os.system('bsub -q %s -o %s %s', (queue, script.replace('.sh', '.log'), script))
+        os.system('bsub -q %s -o %s %s' % (queue, script.replace('.sh', '.log'), script))
 
 def bjobs_setup (argv):
 
@@ -61,23 +61,23 @@ def bjobs_setup (argv):
     if options.madgraph5_pythia8 :
         bjob_list = list()
         import madgraph5_pythia8_plugin
-        bjob_scripts_path = '%s/bjobs/madgraph5_pythia8' % package_path
+        bjob_scripts_path = '%s/bjobs/madgraph5_pythia8/%s' % (package_path, options.tag)
 
         for mcline in decode_txt(options.input):
             mclist = mcline.split()
             mc_job = bjob_scripts_path + '/' + mclist[0]
             os.system('mkdir -p %s' % mc_job)
             for i in range(int(mclist[2])) :
-                outputdir = '%s/output%d' % (options.outdir + '/' + mclist[0], int(i))
+                outputdir = '%s/output%d' % (options.outdir + '/' + options.tag + '/' + mclist[0], int(i))
                 os.system('mkdir -p ' + outputdir)
                 mc_base = package_path + '/Cards/MadGraph5_aMCatNLO/' + mclist[0]
-                if os.path.isfile(mc_base + '/cript.txt') :
-                    print 'Cannot find %s \n' % (mc_base + '/script.txt')
+                if os.path.isfile(mc_base + '/cript.dat') :
+                    print 'Cannot find %s \n' % (mc_base + '/script.dat')
                     exit()
                 USER = os.environ.get('USER')
                 tmp_dir = '/tmp/%s/%s_%s_tmp' % (USER, mclist[0], options.tag)
-                mc_script = '%s/madgraph5_script%d.txt' % (mc_job, int(i))
-                os.system("sed -e 's/MYPATH/%s/" % tmp_dir.replace('/', '\/') + "g' %s > %s" % (mc_base + '/script.txt', mc_script))
+                mc_script = '%s/madgraph5_script%d.dat' % (mc_job, int(i))
+                os.system("sed -e 's/MYPATH/%s/" % tmp_dir.replace('/', '\/') + "g' %s > %s" % (mc_base + '/script.dat', mc_script))
                 os.system("echo 'set nevents %d' >> %s" % (int(mclist[1]), mc_script))
 
                 #Pythia8
@@ -91,8 +91,7 @@ def bjobs_setup (argv):
                     bjob_script.write('\n')
                     if (options.FastSim) :
                         bjob_script.write('gunzip %s/Events/run_01/tag_1_pythia8_events.hepmc.gz\n' % tmp_dir)
-                        bjob_script.write("$HEPTOOL/delphes_install/bin/DelphesHepMC $HEPTOOL/%s \ \n" % options.delphes_card)
-                        bjob_script.write("    %s/delphes_output.root %s/Events/run_01/tag_1_pythia8_events.hepmc\n\n" % (outputdir, tmp_dir))
+                        bjob_script.write("$HEPTOOL/delphes_install/bin/DelphesHepMC $VFS_PACKAGE_PATH/%s %s/delphes_output.root %s/Events/run_01/tag_1_pythia8_events.hepmc\n\n" % (options.delphes_card, outputdir, tmp_dir))
                         for filetype in ['txt', 'dat', 'log', 'gnuplot', 'HwU'] :
                             bjob_script.write('cp %s/Events/run_01/*.%s %s/\n' % (tmp_dir, filetype, outputdir))
                         bjob_script.write('rm -rf %s' % tmp_dir)
